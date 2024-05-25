1d08678b9ab01070e83bb3a4ccd3d4b609fd499d
ahlaiptek <iptekahla@gmail.com>
2024-05-25 09:38:21 +0700

    menghapus semua dan mencoba reactjs cdn

--- src/partials/Header.jsx
+++
@@ -1,9 +0,0 @@
-function Header(props) {
-    return (
-        <div className="alert alert-primary text-center" role="alert">
-            {props.text}
-        </div>
-    )
-}
-
-export default Header
